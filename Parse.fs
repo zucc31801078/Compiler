375969df4207ff9a81ba4af792a7afcb3599949a
beyondor <48287903+beyondor@users.noreply.github.com>
2021-06-22 22:40:38 +0800

    Add files via upload

--- Parse.fs
+++ Parse.fs
@@ -14,7 +14,7 @@ open Debug
 let fromString (str : string) : program =
     let lexbuf = Lexing.LexBuffer<char>.FromString(str)
     try 
-      CPar.Main CLex.Token lexbuf
+      MyCPar.Main MyCLex.Token lexbuf
     with 
       | exn -> let pos = lexbuf.EndPos 
                failwithf "%s near line %d, column %d\n" 
@@ -23,10 +23,10 @@ let fromString (str : string) : program =
 // 词法分析程序，info 在调试的时候被调用，显示Token
 // CLex.Token 词法分析程序入口
 let token buf = 
-    let res = CLex.Token buf
+    let res = MyCLex.Token buf
     msg <|
           match res with
-           |CPar.EOF -> sprintf "%A\n" res
+           |MyCPar.EOF -> sprintf "%A\n" res
            |_ -> sprintf "%A, " res
            
     res
@@ -38,7 +38,7 @@ let fromFile (filename : string) =
       msg "\nToken:\n"
       
       //CPar.Main  语法分析主程序 
-      let ast = CPar.Main token lexbuf in
+      let ast = MyCPar.Main MyCLex.Token lexbuf in
         msg "\nAST:\n";
         ast
     with 
